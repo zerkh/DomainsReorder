9446def3f136ebb7feaed5b0fa2b6fd9dbf1acc9
zerkh <mhzx20@163.com>
2015-10-14 19:59:40 +0800

    10/14 19:25 add wordvectors derivation

--- nn/lbfgstrainer.py
+++ nn/lbfgstrainer.py
@@ -238,7 +238,7 @@ def compute_cost_and_grad(theta, instances, word_vectors, embsize, total_interna
             offset = RecursiveAutoencoder.compute_parameter_num(embsize)
             rm = ReorderClassifer.build(theta[offset:], embsize, rae)
             offset += ReorderClassifer.compute_parameter_num(embsize)
-            word_vectors.reloadVectors(theta[offset:])
+            word_vectors = word_vectors.reloadVectors(theta[offset:])
 
             # compute local reconstruction error, reo and gradients
             local_rae_error, local_rm_error,rae_rec_gradient, rae_gradient, rm_gradient, wordvector_gradient \
