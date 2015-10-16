acf1ffc06475ca8ea108cdb77ace6b2f07ac35ec
zerkh <mhzx20@163.com>
2015-10-16 21:29:27 +0800

    10/16 21:29

--- mpi-train.sh
+++ mpi-train.sh
@@ -15,13 +15,11 @@ mpirun -n $1 python $PYTHONPATH/nn/lbfgstrainer.py\
   -instances_of_Spoken $DEMODIR/data/Spoken/train/dev_trad_me_reorder.xml\
   -instances_of_Science $DEMODIR/data/Science/train/dev_trad_me_reorder.xml\
   -instances_of_News $DEMODIR/data/MixedDomain/test/nist03/trad_me_reorder.xml\
-  -instances_of_Unlabel $DEMODIR/data/MixedDomain/dev/unlabel.src\
   -model $DEMODIR/output/training-file.mpi-$N.model.gz\
   -word_vector $DEMODIR/data/MixedDomain/MixedDomains.zh.vec\
-  -lambda_rec 0.15\
-  -lambda_reg 0.15\
-  -lambda_reo 0.15\
-  -lambda_unlabel 0.15\
-  -isTest 0\
-  -m 200\
-  -v 0\
+  -lambda_reg 0.00006\
+  -lambda_reo 0.88236\
+  -lambda_rec 0.11764\
+  -isTest 1\
+  -m 100\
+  -v 1
